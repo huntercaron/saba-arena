0bb591a077c1eda5054d06df52df60618dfa2167
Hunter Caron <huntercaron@me.com>
2021-09-13 21:40:58 +0200

    adding more sites

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,34 @@
 import Head from "next/head"
 import { getArenaChannel } from "../lib/api"
 
-
 // Grabs all the blocks from the are.na channel and repeats the "LinkItem" component per block
 // See all the block attributes here https://dev.are.na/documentation/blocks#Block52094
 
 function LinkItem(props) {
     const { block } = props
 
-    const isBlockLink =
-        block.source.hasOwnProperty("title") && block.source.hasOwnProperty("url")
+    if (!block?.source) return null
+
+    const isBlockLink = "title" in block.source && "url" in block.source
 
     if (!isBlockLink) return null
 
     return (
         <li className="link-item">
-            <a className="inline-block" href={block.source.url} target="_blank" rel="noopener noreferrer">
+            <a
+                className="inline-block"
+                href={block.source.url}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 {block.generated_title}
             </a>
 
             <p className="inline-block description">{block.description}</p>
 
-            <p className="inline-block time">{block.created_at.split("T")[0]}</p>
+            <p className="inline-block time">
+                {block.created_at.split("T")[0]}
+            </p>
         </li>
     )
 }
